1e80a934499dc9c63ef745809aff557adc3895f7
KevinW <bird831205@hotmail.com>
2019-06-01 13:57:34 -0400

    initial commit

---
+++ client/build/precache-manifest.898c273f4cdf7605fcacadbb3e2177a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de205701c95eb6f3dbdb",
+    "url": "/static/js/main.1788bd72.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "de205701c95eb6f3dbdb",
+    "url": "/static/css/main.77fee1bb.chunk.css"
+  },
+  {
+    "revision": "eed906f23c0aecee96e4bdbbe4284e87",
+    "url": "/index.html"
+  }
+];
